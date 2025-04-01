5d0170a8f9a3ab4ded473ca502718675199e748f
Swati Goyal <swati.goyal@jiva.ag>
2025-04-01 10:00:16 +0530

    hamming

--- hamming/hamming.py
+++ hamming/hamming.py
@@ -1,2 +1,5 @@
 def distance(strand_a, strand_b):
-    pass
+    if len(strand_a) != len(strand_b):
+        raise ValueError("Strands must be of equal length.")
+
+    return sum([1 for i, j in zip(strand_a, strand_b) if i != j])
